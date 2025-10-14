20c93a0b835cd54a38f37dea882c3dc0f7c167c1
longlee <131141761+ThanhLog@users.noreply.github.com>
2025-10-14 16:02:18 +0700

    fix bugs

--- AI/.venv/Scripts/activate.bat
+++ AI/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\ChuyenDoiSo\AI\.venv
+set VIRTUAL_ENV=C:\Users\ADMIN\My-project\smes_project\AI\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
